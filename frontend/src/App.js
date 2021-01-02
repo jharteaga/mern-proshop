cf4f708cc524944db1749e4656c3527126947f64
jharteaga <jh.arteaga09@gmail.com>
2021-01-01 22:49:47 -0800

    add navbar, footer with react-bootstrap components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,19 @@
-import Footer from './components/Footer';
-import Header from './components/Header';
+import { Container } from 'react-bootstrap'
+import Footer from './components/Footer'
+import Header from './components/Header'
 
 const App = () => {
-  return (
-    <>
-      <Header />
-      <main>
-        <h1>Welcome To ProShop</h1>
-      </main>
-      <Footer />
-    </>
-  );
-};
+	return (
+		<>
+			<Header />
+			<main className="py-3">
+				<Container>
+					<h1>Welcome To ProShop</h1>
+				</Container>
+			</main>
+			<Footer />
+		</>
+	)
+}
 
-export default App;
+export default App
